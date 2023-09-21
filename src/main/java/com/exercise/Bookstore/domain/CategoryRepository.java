473dc3628d991e78292b9abc838369845b90e84a
Iuliia <ybe2004ybe@gmail.com>
2023-09-21 13:07:54 +0300

    search by name

--- src/main/java/com/exercise/Bookstore/domain/CategoryRepository.java
+++ src/main/java/com/exercise/Bookstore/domain/CategoryRepository.java
@@ -3,9 +3,10 @@ package com.exercise.Bookstore.domain;
 import java.util.List;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 public interface CategoryRepository extends CrudRepository<Category, Long> {
 
-    List<Category> findByName(String name);
+    List<Category> findByName(@Param("name") String name);
     
 }
