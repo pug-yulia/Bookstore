89ae334aa6c7d29486c68ab5547496be5766984b
Iuliia <ybe2004ybe@gmail.com>
2023-09-21 13:10:46 +0300

    forgot smth

--- src/main/java/com/exercise/Bookstore/domain/BookRepository.java
+++ src/main/java/com/exercise/Bookstore/domain/BookRepository.java
@@ -3,10 +3,11 @@ package com.exercise.Bookstore.domain;
 import java.util.List;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 
 public interface BookRepository extends CrudRepository<Book, Long> {
 
-    List<Book> findByIsbn(String isbn);
+    List<Book> findByIsbn(@Param("isbn") String isbn);
     
 }
